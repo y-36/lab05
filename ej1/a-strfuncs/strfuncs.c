6561e1e3e6e0dc269c281743938393be9b831958
Yung_Threesix <aaron.aravena@mi.unc.edu.ar>
2024-05-11 03:33:16 -0300

    *str

---
+++ ej1/a-strfuncs/strfuncs.c
@@ -0,0 +1,81 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "strfuncs.h"
+
+/*Implement *str where*/
+size_t string_length(const char *str){
+/*Calcula la longitud de la cadena apuntada por str*/
+   int s;
+   if(str == NULL){
+	printf("NULL\n");
+         s=0;
+	return 0;
+    } 
+    size_t i = 1;
+    while (*str != '\0') {
+        i++;
+        str++;
+    }
+    return i;
+}
+
+char *string_filter(const char *str, char c){
+/*Devueleve una nueva cadena en memoria dinamina
+  se optine tomando los char de str !=c */
+    if (str == NULL) {
+        printf("NULL\n");
+        return NULL; // Devuelve un puntero nulo
+    }
+    // Calcula la longitud de la cadena original
+    size_t len = string_length(str);
+    // Asigna memoria dinámica para la nueva cadena
+    char *filtered_str = (char *)malloc(len + 1); // +1 para el carácter nulo
+    if (filtered_str != NULL) {
+        size_t i = 0;
+        size_t j = 0;
+        // Copia los caracteres que no son igual a 'c'
+        while (str[i] != '\0') {
+            if (str[i] != c) {
+                filtered_str[j] = str[i];
+                j++;
+            }
+            i++;
+        }
+        // Agrega el carácter nulo al final
+        filtered_str[j] = '\0';
+    }
+    return filtered_str;
+}
+
+bool string_is_symmetric(const char *str){
+/*Indica si la cadena apuntada por str es simetrica
+   ejemplos "aba" "abcba, "a", "".*/
+    if (str == NULL) {
+        return false; // Manejo de puntero nulo
+    }
+
+    // Calcula la longitud de la cadena sin usar strlen
+    size_t tamano = 0;
+    while (str[tamano] != '\0') {
+        tamano++;
+    }
+
+    // Inicializa los índices para recorrer la cadena
+    int inicio = 0;
+    int fin = tamano - 1;
+
+    // Compara los caracteres desde los extremos hacia el centro
+    while (inicio < fin) {
+        if (str[inicio] != str[fin]) {
+            return false; // No es simétrica
+        }
+        inicio++;
+        fin--;
+    }
+
+    return true;
+}
+
---
+++ ej1/a-strfuncs/strfuncs.h
@@ -0,0 +1,16 @@
+#ifndef _STRFUNCS_H
+#define _STRFUNCS_H
+#include <stdbool.h>
+
+size_t string_length(const char *str);
+/*Calcula la longitud de la cadena apuntada por str*/
+
+char *string_filter(const char *str, char c);
+/*Devueleve una nueva cadena en memoria dinamina
+  se optine tomando los char de str !=c*/
+
+bool string_is_symmetric(const char *str);
+/*Indica si la cadena apuntada por str es simetrica
+   ejemplos "aba" "abcba, "a", "".*/
+
+#endif
