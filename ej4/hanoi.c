ecdc970e8ccbc0595b79bfbf098786c6b4ceee91
Aaron Lihuel Aravena <aaron.aravena@mi.unc.edu.ar>
2024-05-21 04:19:33 -0300

    hanoi_init

--- ej4/hanoi.c
+++ ej4/hanoi.c
@@ -27,7 +27,8 @@ hanoi_t hanoi_init(unsigned int disk_count) {
     hanoi_t hanoi = malloc(sizeof(struct _hanoi));
     assert(hanoi != NULL);
     hanoi->aux = stack_empty();
-    hanoi->target = NULL;
+    hanoi->target = stack_empty();
+    hanoi->source = stack_empty();
     hanoi->disk_count = disk_count;
     for (unsigned int i = disk_count; i > 0; --i) {
         hanoi->source = stack_push(hanoi->source, i);
